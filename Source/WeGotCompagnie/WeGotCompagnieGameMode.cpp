b2b95d6486af9c51268027201ef97b95ec26a82d
HungVu810 <hungdv810@gmail.com>
2022-10-14 22:21:40 -0700

    Added draft binding functions to handle controller inputs in the BP_ThirdPersonCharacter Blueprint. Added materials for highlighting object (MF_SamplingShiftedTexCoord and MT_Outliner). Added new reset event to respawn player at designated location in the Level Blueprint ThirdPersonMap. Added some useful macros in WeGotCompagnieGameMode.cpp to print debugging logs or messages. Added draft TubaBoss Blueprint and Behavior Tree. Added a second PlayerStart actor in the level for the boss' spawn position.

--- Source/WeGotCompagnie/WeGotCompagnieGameMode.cpp
+++ Source/WeGotCompagnie/WeGotCompagnieGameMode.cpp
@@ -3,6 +3,21 @@
 #include "WeGotCompagnieGameMode.h"
 #include "WeGotCompagnieCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerStart.h"
+
+// Should move the macros into a seperate HelperMacros class
+// TEXT() is used for FName, TChar and FString
+// FName can generate FString with ToString
+// Fstring must be dereferenced to TChar* for formated printing to avoid compilation issue
+#define Print(fstring) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, fstring)
+// Normal log
+#define Log(tchar) UE_LOG(LogTemp, Warning, tchar)
+// Formatted log
+#define Logf(tchar_format, ...) UE_LOG_REF(LogTemp, Warning, tchar_format, ##__VA_ARGS__)
+#define BoolToText(boolean) boolean ? TEXT("true") : TEXT("false")
+#define GetVarName(var) TEXT(#var)
+#define CheckNull(ptr) if(!ptr) { Print(FString::Printf(TEXT("%s is an nullptr"), GetVarName(var))); return; }
 
 AWeGotCompagnieGameMode::AWeGotCompagnieGameMode()
 {
@@ -13,3 +28,4 @@ AWeGotCompagnieGameMode::AWeGotCompagnieGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
